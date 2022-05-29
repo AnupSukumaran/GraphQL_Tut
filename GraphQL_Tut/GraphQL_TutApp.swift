852c8613f80c1ac8098883d0be443e5844e14ee7
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2022-05-29 15:16:07 +0100

    Initial Commit

---
+++ GraphQL_Tut/GraphQL_TutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_TutApp.swift
+//  GraphQL_Tut
+//
+//  Created by Sukumar Anup Sukumaran on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_TutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GraphQL_TutUITests/GraphQL_TutUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GraphQL_TutUITestsLaunchTests.swift
+//  GraphQL_TutUITests
+//
+//  Created by Sukumar Anup Sukumaran on 29/05/2022.
+//
+
+import XCTest
+
+class GraphQL_TutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
